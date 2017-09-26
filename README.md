3dce34ed1741c93a6d019521a19a683697101996
Piotr Kowalski <piecioshka@gmail.com>
2017-09-26 10:29:57 +0200

    Build search input

---
+++ README.md
@@ -0,0 +1,13 @@
+# test-search-input-with-close-button
+
+> :ledger: Build input with movable clear button
+
+## Preview
+
+https://piecioshka.github.io/test-search-input-with-close-button/
+
+![](./demo.gif)
+
+## License
+
+[The MIT License](http://piecioshka.mit-license.org) @ 2017
---
+++ script.js
@@ -0,0 +1,38 @@
+const $span = document.querySelector('span');
+
+function clearInputAfterClickButton(element) {
+    element.addEventListener('click', (evt) => {
+        if (evt.offsetX > element.offsetWidth) {
+            element.innerText = '';
+        }
+    });
+}
+
+function applyLimitOfCharacters(element) {
+    element.addEventListener('keypress', (evt) => {
+        if (element.innerText.length > 50) {
+            evt.preventDefault();
+        }
+    });
+}
+
+function setFocusOnElement(element) {
+    element.focus();
+}
+
+function applyMoveTextToTheRight(element) {
+    element.addEventListener('keypress', () => {
+        const antiWidth = -1 *
+            parseInt(window.getComputedStyle(element).width);
+
+        if (element.innerText.length > 40) {
+            console.log('more than 40 chars', antiWidth);
+            element.style['text-indent'] = antiWidth + 'px';
+        }
+    })
+}
+
+clearInputAfterClickButton($span);
+applyLimitOfCharacters($span);
+// setFocusOnElement($span);
+// applyMoveTextToTheRight($span);
